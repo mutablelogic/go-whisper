49ca0948dd9220bcfbfed1c95864a768f39d4d9e
hi_yueban <yuebanlaosiji@outlook.com>
2023-04-13 21:23:34 +0800

    Update streamer.go

--- cmd/stream/streamer.go
+++ cmd/stream/streamer.go
@@ -1,55 +1,82 @@
 package main
 
 import (
+	"unsafe"
+
 	sdl "github.com/veandco/go-sdl2/sdl"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
 // TYPES
 
-type Streamer struct{}
+type StreamerContext struct {
+	Dev  sdl.AudioDeviceID
+	Spec sdl.AudioSpec
+	u8   []byte
+	f32  *float32
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // LIFECYCLE
 
-func NewStreamer() (*Streamer, error) {
-	if err := sdl.Init(sdl.INIT_AUDIO); err != nil {
-		return nil, err
+func NewStreamerContext(dev sdl.AudioDeviceID, spec sdl.AudioSpec) (*StreamerContext, error) {
+	streamer := &StreamerContext{
+		Dev:  dev,
+		Spec: spec,
+		u8:   make([]byte, spec.Size),
+		f32:  (*float32)(unsafe.Pointer(&spec.Silence)),
 	}
-	sdl.LogSetPriority(sdl.LOG_CATEGORY_APPLICATION, sdl.LOG_PRIORITY_INFO)
-	sdl.SetHintWithPriority(sdl.HINT_AUDIO_RESAMPLING_MODE, "medium", sdl.HINT_OVERRIDE)
 
-	return &Streamer{}, nil
+	// Return success
+	return streamer, nil
 }
 
-func (streamer *Streamer) Close() error {
-	sdl.Quit()
+func (streamer *StreamerContext) Close() error {
+	sdl.CloseAudioDevice(streamer.Dev)
+	streamer.Dev = 0
+	streamer.u8 = nil
+	streamer.f32 = nil
 	return nil
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS
 
-// AudioDevices returns a map of audio devices
-func (Streamer) AudioDevices() map[int]string {
-	num := sdl.GetNumAudioDevices(true)
-	result := make(map[int]string, num)
-	for i := 0; i < num; i++ {
-		result[i] = sdl.GetAudioDeviceName(i, true)
-	}
-	return result
+// Start capturing audio
+func (streamer *StreamerContext) Start() {
+	sdl.PauseAudioDevice(streamer.Dev, false)
 }
 
-// Open returns a capture context
-func (Streamer) Open(dev int, rate int32, ch uint8, samples uint16) (*StreamerContext, error) {
-	var requested, obtained sdl.AudioSpec
-	requested.Freq = rate
-	requested.Format = sdl.AUDIO_F32
-	requested.Channels = ch
-	requested.Samples = samples
-	if id, err := sdl.OpenAudioDevice(sdl.GetAudioDeviceName(dev, true), true, &requested, &obtained, 0); err != nil {
-		return nil, err
-	} else {
-		return NewStreamerContext(id, obtained)
+// Stop capturing audio
+func (streamer *StreamerContext) Stop() {
+	sdl.PauseAudioDevice(streamer.Dev, true)
+}
+
+// Return the number of samples in the audio queue
+func (streamer *StreamerContext) NumSamples() uint32 {
+	// Number of samples is the number of bytes divided by the number of bytes per sample (float32)
+	return sdl.GetQueuedAudioSize(streamer.Dev) >> 2
+}
+
+// Dequeue audio from the queue, return nil if not enough audio to fill the buffer
+func (streamer *StreamerContext) Samples() ([]float32, error) {
+	// Check for audio data
+	n := sdl.GetQueuedAudioSize(streamer.Dev)
+	if n < uint32(len(streamer.u8)) {
+		return nil, nil
 	}
+
+	// Dequeue the audio
+	if _, err := sdl.DequeueAudio(streamer.Dev, streamer.u8); err != nil {
+		return nil, err
+	}
+
+	// Calculate the number of samples and return a slice with that length
+	numSamples := n >> 2
+	return (*[1<<31 - 1]float32)(unsafe.Pointer(&streamer.u8[0]))[:numSamples], nil
+}
+
+// Clear any queued audio
+func (streamer *StreamerContext) Clear() {
+	sdl.ClearQueuedAudio(streamer.Dev)
 }
