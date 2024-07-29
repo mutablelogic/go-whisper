0fb4c490a271390861e464f4c61a4a0cbf0e1812
David Thorpe <djt@mutablelogic.com>
2024-07-29 13:23:22 +0200

    Updated dependencies

--- sys/whisper/generate.go
+++ sys/whisper/generate.go
@@ -7,7 +7,7 @@ package whisper
 #cgo pkg-config: libwhisper
 #cgo darwin pkg-config: libwhisper-darwin
 #cgo linux pkg-config: libwhisper-linux
-#cgo cuda pkg-config: libcuda
+#cgo arm64 pkg-config: cuda-12.2 cublas-12.2 cudart-12.2
 */
 import "C"
 
@@ -15,4 +15,4 @@ import "C"
 // Setting the prefix to the base of the repository
 //go:generate go run ../pkg-config --version "0.0.0" --prefix "../.." --cflags "-I$DOLLAR{prefix}/third_party/whisper.cpp/include -I$DOLLAR{prefix}/third_party/whisper.cpp/ggml/include" --libs "-L$DOLLAR{prefix}/third_party/whisper.cpp -lwhisper -lggml -lm -lstdc++" libwhisper.pc
 //go:generate go run ../pkg-config --version "0.0.0" --libs "-framework Accelerate -framework Metal -framework Foundation -framework CoreGraphics" libwhisper-darwin.pc
-//go:generate go run ../pkg-config --version "0.0.0" --libs "-lgomp1" libwhisper-linux.pc
+//go:generate go run ../pkg-config --version "0.0.0" --libs "-lgomp" libwhisper-linux.pc
