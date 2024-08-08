80e13adf8294894134055d54ffad8af646f5d98f
David Thorpe <djt@mutablelogic.com>
2024-08-08 10:02:13 +0200

    Updated

--- sys/whisper/generate_cuda.go
+++ sys/whisper/generate_cuda.go
@@ -7,6 +7,6 @@ package whisper
 
 /*
 #cgo pkg-config: cuda-12.6 cublas-12.6 cudart-12.6
-#cgo arm64 ldflags: -L/usr/local/cuda/lib64/stubs -lcuda
+#cgo LDFLAGS: -L/usr/local/cuda/lib64/stubs -lcuda
 */
 import "C"
