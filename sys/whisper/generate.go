96e5297b9046271d4bbb81569c5ab12b7e7f5d49
David Thorpe <djt@mutablelogic.com>
2024-08-11 07:30:38 +0200

    Updated

--- sys/whisper/generate.go
+++ sys/whisper/generate.go
@@ -6,12 +6,10 @@ package whisper
 /*
 #cgo pkg-config: libwhisper
 #cgo darwin pkg-config: libwhisper-darwin
-#cgo linux pkg-config: libwhisper-linux
 */
 import "C"
 
 // Generate the whisper pkg-config files
 // Setting the prefix to the base of the repository
-//go:generate go run ../pkg-config --version "0.0.0" --prefix "../.." --cflags "-I$DOLLAR{prefix}/third_party/whisper.cpp/include -I$DOLLAR{prefix}/third_party/whisper.cpp/ggml/include" --libs "-L$DOLLAR{prefix}/third_party/whisper.cpp -lwhisper -lggml -lm -lstdc++" libwhisper.pc
+//go:generate go run ../pkg-config --version "0.0.0" --prefix "../.." --cflags "-I$DOLLAR{prefix}/third_party/whisper.cpp/include -I$DOLLAR{prefix}/third_party/whisper.cpp/ggml/include" --libs "-L$DOLLAR{prefix}/third_party/whisper.cpp -lwhisper -lggml -lgomp -lm -lstdc++" libwhisper.pc
 //go:generate go run ../pkg-config --version "0.0.0" --libs "-framework Accelerate -framework Metal -framework Foundation -framework CoreGraphics" libwhisper-darwin.pc
-//go:generate go run ../pkg-config --version "0.0.0" --cflags "-fopenmp" --libs "-lgomp" libwhisper-linux.pc
